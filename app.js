ed65c836422b186260a0bd5edfd3e2481d088527
atharvakulkarni <askulkarni1@sheffield.ac.uk>
2024-03-16 22:21:36 +0000

    Initial commit of home page

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
