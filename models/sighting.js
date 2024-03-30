b986cc8859a64fd85c932bca5cf260b63c540736
rain433 <www.ipod.liyucheng@foxmail.com>
2024-03-30 14:45:55 +0000

    change the database

--- models/sighting.js
+++ models/sighting.js
@@ -82,6 +82,7 @@ let SightingSchema = new mongoose.Schema({
 // );
 
 
+
 // Configure the 'toObject' option for the schema to include getters and virtuals convert to an object.
 SightingSchema.set('toObject', { getters: true, virtuals: true });
 
