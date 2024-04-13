db1c492335afb2148f9a4e6a3fd6e7e732184520
atharvakulkarni <askulkarni1@sheffield.ac.uk>
2024-04-13 19:55:44 +0100

    Added offline storage for home page with cards

---
+++ public/javascripts/idb-utility.js
@@ -0,0 +1,169 @@
+// Function to handle adding a new todo
+const addNewPlantsToSync = (syncTodoIDB, plantData) => {
+    // Retrieve todo text and add it to the IndexedDB
+        console.log("In plants sync!!!")
+        console.log("Plant data: ",plantData)
+        const transaction = syncTodoIDB.transaction(["sync-plants"], "readwrite")
+        const plantStore = transaction.objectStore("sync-plants")
+        const addRequest = plantStore.add(plantData)
+        addRequest.addEventListener("success", () => {
+            console.log("Added " + "#" + addRequest.result)
+            const getRequest = plantStore.get(addRequest.result)
+            getRequest.addEventListener("success", () => {
+                console.log("Found " + JSON.stringify(getRequest.result))
+                // Send a sync message to the service worker
+                navigator.serviceWorker.ready.then((sw) => {
+                    sw.sync.register("sync-plant")
+                }).then(() => {
+                    console.log("Sync registered");
+                }).catch((err) => {
+                    console.log("Sync registration failed: " + JSON.stringify(err))
+                })
+            })
+        })
+
+}
+
+// Function to add new todos to IndexedDB and return a promise
+const addNewTodosToIDB = (plantIDB, plants) => {
+    return new Promise((resolve, reject) => {
+        const transaction = plantIDB.transaction(["plants"], "readwrite");
+        const plantStore = transaction.objectStore("plants");
+
+        const addPromises = plants.map(plant => {
+            return new Promise((resolveAdd, rejectAdd) => {
+                const addRequest = plantStore.add(plant);
+                addRequest.addEventListener("success", () => {
+                    console.log("Added " + "#" + addRequest.result + ": " + plant);
+                    const getRequest = plantStore.get(addRequest.result);
+                    getRequest.addEventListener("success", () => {
+                        console.log("Found " + JSON.stringify(getRequest.result));
+                        insertPlantInCarousel(getRequest.result);
+                        resolveAdd(); // Resolve the add promise
+                    });
+                    getRequest.addEventListener("error", (event) => {
+                        rejectAdd(event.target.error); // Reject the add promise if there's an error
+                    });
+                });
+                addRequest.addEventListener("error", (event) => {
+                    rejectAdd(event.target.error); // Reject the add promise if there's an error
+                });
+            });
+        });
+
+        // Resolve the main promise when all add operations are completed
+        Promise.all(addPromises).then(() => {
+            resolve();
+        }).catch((error) => {
+            reject(error);
+        });
+    });
+};
+
+
+// Function to remove all todos from idb
+const deleteAllExistingTodosFromIDB = (plantIDB) => {
+    const transaction = plantIDB.transaction(["plants"], "readwrite");
+    const plantStore = transaction.objectStore("plants");
+    const clearRequest = plantStore.clear();
+
+    return new Promise((resolve, reject) => {
+        clearRequest.addEventListener("success", () => {
+            resolve();
+        });
+
+        clearRequest.addEventListener("error", (event) => {
+            reject(event.target.error);
+        });
+    });
+};
+
+
+
+
+// Function to get the todo list from the IndexedDB
+const getAllPlants = (plantIDB) => {
+    return new Promise((resolve, reject) => {
+        const transaction = plantIDB.transaction(["plants"]);
+        const plantStore = transaction.objectStore("plants");
+        const getAllPlants = plantStore.getAll();
+
+        // Handle success event
+        getAllPlants.addEventListener("success", (event) => {
+            resolve(event.target.result); // Use event.target.result to get the result
+        });
+
+        // Handle error event
+        getAllPlants.addEventListener("error", (event) => {
+            reject(event.target.error);
+        });
+    });
+}
+
+
+// Function to get the todo list from the IndexedDB
+const getAllSyncPlants = (syncPlantIDB) => {
+    return new Promise((resolve, reject) => {
+        const transaction = syncPlantIDB.transaction(["sync-plants"]);
+        const plantStore = transaction.objectStore("sync-plants");
+        const getAllRequest = plantStore.getAll();
+
+        getAllRequest.addEventListener("success", () => {
+            resolve(getAllRequest.result);
+        });
+
+        getAllRequest.addEventListener("error", (event) => {
+            reject(event.target.error);
+        });
+    });
+}
+
+// Function to delete a syn
+const deleteSyncPlantFromIDB = (syncPlantIDB, id) => {
+    const transaction = syncPlantIDB.transaction(["sync-plants"], "readwrite")
+    const plantStore = transaction.objectStore("sync-plants")
+    const deleteRequest = plantStore.delete(id)
+    deleteRequest.addEventListener("success", () => {
+        console.log("Deleted " + id)
+    })
+}
+
+function openPlantsIDB() {
+    return new Promise((resolve, reject) => {
+        const request = indexedDB.open("plants", 1);
+
+        request.onerror = function (event) {
+            reject(new Error(`Database error: ${event.target}`));
+        };
+
+        request.onupgradeneeded = function (event) {
+            const db = event.target.result;
+            db.createObjectStore('plants', {keyPath: 'id',autoIncrement:true});
+        };
+
+        request.onsuccess = function (event) {
+            const db = event.target.result;
+            resolve(db);
+        };
+    });
+}
+
+function openSyncPlantsIDB() {
+    return new Promise((resolve, reject) => {
+        const request = indexedDB.open("sync-plants", 1);
+        console.log("In openpnats sync idb")
+        request.onerror = function (event) {
+            reject(new Error(`Database error: ${event.target}`));
+        };
+
+        request.onupgradeneeded = function (event) {
+            const db = event.target.result;
+            db.createObjectStore('sync-plants', {keyPath: 'id', autoIncrement: true});
+        };
+
+        request.onsuccess = function (event) {
+            const db = event.target.result;
+            resolve(db);
+        };
+    });
+}
---
+++ public/sw.js
@@ -0,0 +1,114 @@
+importScripts('/javascripts/idb-utility.js');
+
+
+// Use the install event to pre-cache all initial resources.
+self.addEventListener('install', event => {
+    console.log('Service Worker: Installing....');
+    event.waitUntil((async () => {
+
+        console.log('Service Worker: Caching App Shell at the moment......');
+        try {
+            const cache = await caches.open("static");
+            const uploadsFolderUrl = `/api/uploads-list`;
+            const uploadsFolderResponse = await fetch(uploadsFolderUrl);
+            const uploadedFiles = await uploadsFolderResponse.json();
+
+            const imagesToCache = uploadedFiles
+                .map(file => `${self.location.origin}${file}`)
+            console.log(imagesToCache)
+            cache.addAll([
+                '/',
+                '/addplant',
+                '/manifest.json',
+                '/javascripts/API.js',
+                '/javascripts/index.js',
+                '/javascripts/idb-utility.js',
+                'https://maps.googleapis.com/maps/api/js?key=AIzaSyARXO1sAXfsUdl_wxOfVJFFT3naSjyyoII&callback=initMap',
+                '/stylesheets/style.css',
+                '/stylesheets/partials/header_style.css',
+                '/stylesheets/homepage/index.css',
+                'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js',
+                'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js',
+                '/css/bootstrap.min.css',
+                '/images/logo/botanical-lens-logo.png',
+                '/images/red-tick.jpg',
+                '/images/blue_tick.png',
+                '/images/add-plant.svg',
+                ...imagesToCache,
+            ]);
+            console.log("Uploaded files: ",uploadedFiles)
+            console.log('Service Worker: App Shell Cached');
+        }
+        catch{
+            console.log("error occured while caching...")
+        }
+
+    })());
+});
+
+//clear cache on reload
+self.addEventListener('activate', event => {
+// Remove old caches
+    event.waitUntil(
+        (async () => {
+            const keys = await caches.keys();
+            return keys.map(async (cache) => {
+                if(cache !== "static") {
+                    console.log('Service Worker: Removing old cache: '+cache);
+                    return await caches.delete(cache);
+                }
+            })
+        })()
+    )
+})
+
+// Fetch event to fetch from cache first
+self.addEventListener('fetch', event => {
+    event.respondWith((async () => {
+        const cache = await caches.open("static");
+        const cachedResponse = await cache.match(event.request);
+        if (cachedResponse) {
+            console.log('Service Worker: Fetching from Cache: ', event.request.url);
+            return cachedResponse;
+        }
+        console.log('Service Worker: Fetching from URL: ', event.request.url);
+        return fetch(event.request);
+    })());
+});
+
+//Sync event to sync the todos
+self.addEventListener('sync', event => {
+    if (event.tag === 'sync-plant') {
+        console.log('Service Worker: Syncing new Plants');
+        openSyncPlantsIDB().then((syncPostDB) => {
+            getAllSyncPlants(syncPostDB).then((syncPlants) => {
+                for (const syncPlant of syncPlants) {
+                    console.log('Service Worker: Syncing new Plant: ', syncPlant);
+                    console.log(syncPlant)
+                    const formData =  new FormData();
+                    // Append each property of syncPlant to the FormData object
+                    for (const key in syncPlant) {
+                        formData.append(key, syncPlant[key]);
+                    }
+
+                    console.log("Form data : ",formData)
+
+                    // Fetch with FormData instead of JSON
+                    fetch('http://localhost:3000/addplant', {
+                        method: 'POST',
+                        body: formData
+                    }).then(() => {
+                        console.log('Service Worker: Syncing new Plant: ', syncPlant, ' done');
+                        deleteSyncPlantFromIDB(syncPostDB,syncPlant.id);
+                        // Send a notification
+                        // self.registration.showNotification('Todo Synced', {
+                        //     body: 'Todo synced successfully!',
+                        // });
+                    }).catch((err) => {
+                        console.error('Service Worker: Syncing new Todo: ', syncPlant, ' failed');
+                    });
+                }
+            });
+        });
+    }
+});
