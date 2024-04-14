76323680455c990a8c183b397fc754e5cc809e13
Vivek Shravan Tate <vivektate.developer@gmail.com>
2024-04-14 12:51:28 +0100

    Fixed code in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var API_KEY = require('../public/javascripts/API')
+var path = require('path');
 var Sighting = require('../models/sighting')
 const {create,getAll,getPlantsPagewise} = require('../controllers/sighting');
 const sightingModel = require('../models/sighting');
 const session = require('express-session');
 var multer=require("multer")
+var fs=require("fs")
 /* GET home page. */
 
 router.use(session({
@@ -22,6 +24,7 @@ var storage=multer.diskStorage({
         var original=file.originalname;
         var file_extension=original.split(".")
         filename=Date.now()+"."+file_extension[file_extension.length-1];
+        console.log("File nane: ",filename)
         cb(null,filename);
     }
 });
@@ -36,15 +39,14 @@ function generateUserID() {
 }
 
 router.get('/', async (req, res, next)=> {
-    if (!req.session.user_id) {
-        req.session.user_id = generateUserID();
-    }
-
+    const userId = req.query.user_id || generateUserID();
+    res.locals.user_id = userId;
+    console.log("Routes user id: ",res.locals.user_id)
     const page = parseInt(req.query.page) || 1;
     const limit = 8; // Number of plants per page
 
     // Fetch plant data from the database
-    const plants = await sightingController.getAll(page, limit);
+    const plants = await getPlantsPagewise(page, limit);
 
     // Calculate total number of pages
     const totalPlants = await sightingModel.countDocuments();
@@ -62,11 +64,10 @@ router.get('/addplant', function (req, res, next) {
     res.render('addplant', {title: 'Add Plant',user_id:userId}); // Use 'addplant' as the EJS template file name
 });
 
-router.post('/addplant', upload.single('uploadImage'),async (req, res) => {
-    // Call the create function from controllers/sightings.js
-    let filePath = req.file ? req.file.path : null;
-    await create(req,filePath);
-    res.redirect('/');
+router.post('/addplant',upload.single('uploadImage'),async (req, res) => {
+    console.log("Add plant req body: ",req.body)
+
+    await create(req.body,req.file.path);
 });
 
 // route to get all todos
@@ -82,7 +83,7 @@ router.get('/plants', function (req, res, next) {
 
 router.get('/api/uploads-list', (req, res) => {
     const uploadsDir = path.join(__dirname, '..','public', 'images', 'uploads');
-console.log(uploadsDir)
+    console.log(uploadsDir)
     // Read the directory and get the list of files
     fs.readdir(uploadsDir, (err, files) => {
         if (err) {
@@ -98,6 +99,7 @@ console.log(uploadsDir)
         return res.json(uploadUrls);
     });
 });
+
 router.get('/plantdetails', function (req, res, next) {
     res.render('plantdetails', {title: 'Plant Details'}); // Use 'plantdetails' as the EJS template file name
 });
