fb06c013c953a2f81c1e2d7057dc90b5cf457a04
rain433 <www.ipod.liyucheng@foxmail.com>
2024-04-26 02:54:56 +0100

    cancel the image section to easy the chat function

--- models/Message.js
+++ models/Message.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const messageSchema = new mongoose.Schema({
     username: { type: String, required: true },
     message: { type: String, required: true },
-    timestamp: { type: Date, default: Date.now },
-    imageUrl: { type: String, required: false }
+    timestamp: { type: Date, default: Date.now }
 });
 
 const Message = mongoose.model('Message', messageSchema);
