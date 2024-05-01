d3a33dd2ab157efc90fe7b4600e22887aa9d6035
Atharva Kulkarni <64882461+atharva2109@users.noreply.github.com>
2024-05-01 13:50:08 +0100

    Task/added notifications (#19)
    * Updated .gitignore
    
    * Updated .gitignore again
    
    * Added notifications in plant app
    
    * Removed the uploads folder images
    
    ---------
    
    Co-authored-by: atharvakulkarni <askulkarni1@sheffield.ac.uk>

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,6 +2,8 @@ let isFirstPlantAdded = false;
 let count = 0;
 let isTopFirstPlantAdded = false;
 let currentMapIndex = 0;
+
+
 const insertPlantInCarousel = (plants) => {
     if (plants.plant) {
         const plantList = document.getElementById('plant_list');
@@ -165,8 +167,39 @@ window.onload = function () {
             });
     }
 
-    if (navigator.onLine) {
+    if ("Notification" in window) {
+        // Check if the user has granted permission to receive notifications
+        if (Notification.permission === "granted") {
+            navigator.serviceWorker.ready.then((registration) => {
+                registration.showNotification("Plant Web Application", {
+                    body: "Plant application",
+                    tag: "Plant application",
+                    icon: '/images/logo/Squared_Logo.png'
+                }).then(r =>
+                    console.log(r)
+                );
+            });
+        } else if (Notification.permission !== "denied") {
+            // If the user hasn't been asked yet or has previously denied permission,
+            // you can request permission from the user
+            Notification.requestPermission().then(function (permission) {
+                // If the user grants permission, you can proceed to create notifications
+                if (permission === "granted") {
+                    navigator.serviceWorker.ready
+                        .then(function (serviceWorkerRegistration) {
+                            serviceWorkerRegistration.showNotification("Todo App",
+                                {body: "Notifications are enabled!"})
+                                .then(r =>
+                                    console.log(r)
+                                );
+                        });
+                }
+            });
+        }
+    }
 
+    if (navigator.onLine) {
+        console.log("Online mode")
         fetch('http://localhost:3000/plants')
             .then(function (res) {
                 return res.json();
