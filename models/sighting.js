8a9898d91f32e369300fc30895c25377951f3479
Vivek Shravan Tate <vivektate.developer@gmail.com>
2024-05-02 22:33:13 +0100

    Updated the offline feature.

--- models/sighting.js
+++ models/sighting.js
@@ -8,65 +8,50 @@ let SightingSchema = new Schema(
     {
 
         // Sighting Fields
-        sightingId: { type:Number},
-        date: { type: String },                               // Date of sighting
-        location: { type: String},                       // Location of the sighting
-        address: {
-            line: String,
-            city: String,
-            state: String,
-            country: String,
-            pinCode: String
-        },                                                                  // Address in words
+        sightingId: {type: Number},
+        date: {type: String},                               // Date of sighting
+        location: {type: String},                       // Location of the sighting
+        line: String,
+        city: String,
+        state: String,
+        country: String,
+        pinCode: String,                                                                  // Address in words
         altitude: Number,                                                   // Altitude of the sighting
         status: {
             type: String,
             enum: ['Verified', 'Verification in Progress']
         },                                                                  // Status of the sighting
-
-        // Plant Fields
-        plant: {
-            name: String,                                                   // Plant name
-            commonName: String,                                             // Plant common name
-            scientificName: String,                                         // Plant scientific name
-            family: String,                                                 // Plant family
-            genus: String,                                                  // Plant Genus
-            species: String,                                                // Plant species
-            description: { type: String },                  // Plant/Environment Description
-            size: {
-                length: String,
-                height: String,
-                width: String
-            },                                                              // Plant size
-            characteristics: {
-                flowering: Boolean,
-                hasLeaves: Boolean,
-                fruitBearing: Boolean,
-                sunExposure: {
-                    type: String,
-                    enum: ['Shade', 'Partial Sun', 'Full Sun']
-                },
-                flowerColor: String
-            },                                                              // Plant characteristics
-            identificationLink: String,                                     // Plant identification link
-            photos: [String]                                             // Plant photos
+        name: String,                                                   // Plant name
+        commonName: String,                                             // Plant common name
+        scientificName: String,                                         // Plant scientific name
+        family: String,                                                 // Plant family
+        genus: String,                                                  // Plant Genus
+        species: String,                                                // Plant species
+        description: String,                  // Plant/Environment Description
+        length: String,
+        height: String,
+        width: String,
+        flowering: Boolean,
+        hasLeaves: Boolean,
+        fruitBearing: Boolean,
+        sunExposure: {
+            type: String,
+            enum: ['Shade', 'Partial Sun', 'Full Sun']
         },
+        flowerColor: String,
 
-        // User Fields
-        user: {
-            id: { type: Number },             // user id
-            name: String,                                                   // user name
-            contactDetails: {
-                email: String,
-                phoneNumber: Number
-            }                                                               // user contact details
-        }
-    }
-);
+        // Plant characteristics
+        identificationLink: String,                                     // Plant identification link
+        uploadImage: String,
+        userid: Number,
+        username: String,                                                   // user name
+        email: String,
+        phoneNumber: Number
+    });
 
 
 // Configure the 'toObject' option for the schema to include getters and virtuals convert to an object.
-SightingSchema.set('toObject', { getters: true, virtuals: true });
+SightingSchema.set('toObject', {getters: true, virtuals: true});
 
 // Create the mongoose model 'Sighting' based on the defined schema
 let Sighting = mongoose.model('sighting', SightingSchema);
